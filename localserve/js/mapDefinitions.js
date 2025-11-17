c5b068c3ef2d10b3542498bb3a850f05a21edd0f
Geir Smestad <geir.smestad@gmail.com>
2025-11-18 00:10:06 +0100

    Added new mode that serves maps from back-end & DB rather than file system

---
+++ localserve/js/mapDefinitions.js
@@ -0,0 +1,93 @@
+const mapDefinitions = [
+  {
+    "nw_coords": [
+      60.4413596226974,
+      5.291421604355309
+    ],
+    "se_coords": [
+      60.416058601946006,
+      5.330736657405209
+    ],
+    "map_name": "Munkebotn-2021",
+    "map_filename": "munkebotn_rotated.png",
+    "attribution": ""
+  },
+  {
+    "nw_coords": [
+      60.40908318634827,
+      5.335459558239961
+    ],
+    "se_coords": [
+      60.385976819472006,
+      5.3720671422118995
+    ],
+    "map_name": "Fløyen-2023",
+    "map_filename": "floyen_compressed_70.webp",
+    "attribution": ""
+  },
+  {
+    "nw_coords": [
+      60.45973869078658,
+      5.296984088280552
+    ],
+    "se_coords": [
+      60.442877100508994,
+      5.323230560339303
+    ],
+    "map_name": "Åstveitskogen-tur-O-2024",
+    "map_filename": "2024-astveitskogen-tur-o-rotated.png",
+    "attribution": ""
+  },
+  {
+    "nw_coords": [
+      60.31601104308776,
+      5.233903081840795
+    ],
+    "se_coords": [
+      60.291277752554805,
+      5.2719673393763
+    ],
+    "map_name": "Kokstad-2022",
+    "map_filename": "kokstad-2024-rotated.png",
+    "attribution": ""
+  },
+  {
+    "nw_coords": [
+      60.340253651996576,
+      5.287258573334927
+    ],
+    "se_coords": [
+      60.32621219980405,
+      5.308932248866654
+    ],
+    "map_name": "Bønestoppen-2013",
+    "map_filename": "bonestoppen-rotated.png",
+    "attribution": ""
+  },
+  {
+    "nw_coords": [
+      60.421739083410074,
+      5.3274284422185305
+    ],
+    "se_coords": [
+      60.39645243335368,
+      5.364985139792009
+    ],
+    "map_name": "Midtfjellet-2012",
+    "map_filename": "Midtfjellet-2012.webp",
+    "attribution": ""
+  },
+  {
+    "nw_coords": [
+      60.421505975167136,
+      5.336727549279205
+    ],
+    "se_coords": [
+      60.389417606168365,
+      5.385452992220802
+    ],
+    "map_name": "blamannen-10k",
+    "map_filename": "blamannen-10k.webp",
+    "attribution": ""
+  }
+];
